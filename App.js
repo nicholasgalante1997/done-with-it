aa18cfc004aa2a873dadd05c3f94c0c363a72f05
nicholasgalante1997 <nicholasgalante1997@gmail.com>
2020-10-25 19:09:42 -0400

    half

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React Native</Text>
+      <TouchableOpacity onPress={() => console.log('Ashley has the buds.a')}>
+        <Image source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300"}} />
+       </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
